d4533b0c003989eb65a741e15db7a3df227546f0
Zeliades <fericgarciap1@gmail.com>
2023-07-11 10:41:13 -0400

    Agregar archivos de proyecto.

---
+++ B-Express/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Express POS")]
+[assembly: AssemblyDescription("Express POS Sistema para el Seguimiento de Inventario, Pedidos,Ventas y Entregas")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ing. Héctor Daniel Di Stasi")]
+[assembly: AssemblyProduct("Express POS Sistema de Gestión de Inventario")]
+[assembly: AssemblyCopyright("Copyright © 2021 Ing. Héctor Daniel Di Stasi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee994934-4ef9-4a49-ad71-b2282e5304d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
